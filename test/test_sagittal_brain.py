4e63e3eefe0c8013b8071b99270a0ac4d248252f
Zhexuan <ucapzso@ucl.ac.uk>
2024-11-18 13:04:57 +0000

    Fix error in test

--- test/test_sagittal_brain.py
+++ test/test_sagittal_brain.py
@@ -1,4 +1,5 @@
 import numpy as np
+from sagittal_average import run_averages
 
 data_input = np.zeros((20, 20))
 data_input[-1, :] = 1
@@ -6,6 +7,8 @@ np.savetxt("brain_sample.csv", data_input, fmt="%d", delimiter=",")
 
 expected_output = np.zeros(20)
 expected_output[-1] = 1
+
+run_averages(file_input="brain_sample.csv", file_output="brain_average.csv")
 
 real_output = np.genfromtxt("brain_average.csv", delimiter=",")
 np.testing.assert_array_equal(real_output, expected_output)
