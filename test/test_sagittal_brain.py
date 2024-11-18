0e412791f4049d3dc6ceaf9f920a4ab780402c7d
Zhexuan <ucapzso@ucl.ac.uk>
2024-11-18 10:53:36 +0000

    Add test to package

--- test/test_sagittal_brain.py
+++ test/test_sagittal_brain.py
@@ -4,8 +4,6 @@ data_input = np.zeros((20, 20))
 data_input[-1, :] = 1
 np.savetxt("brain_sample.csv", data_input, fmt="%d", delimiter=",")
 
-import test_call_command
-
 expected_output = np.zeros(20)
 expected_output[-1] = 1
 
